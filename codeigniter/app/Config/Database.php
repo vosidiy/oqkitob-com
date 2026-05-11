86ce568beed19181427218ce1c99d22c022a1eb0
vosidiy <vosidiy@gmail.com>
2026-05-11 20:49:08 +0500

    setup done

--- codeigniter/app/Config/Database.php
+++ codeigniter/app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'localhost', // or 127.0.0.1
+        'username'     => 'root',
+        'password'     => 'root',
+        'database'     => 'oqkitob',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => 8889, // 3306 by default
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
